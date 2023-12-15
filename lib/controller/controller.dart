15ecbade5a32fb0d78934f12a4954aba29925a7e
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2023-12-15 20:17:53 +0700

    fetch API on database

---
+++ lib/controller/controller.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:flutter_application_1/data/album.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<List<Album>> fetchAlbumList() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+
+  if (response.statusCode == 200) {
+    final albums = jsonDecode(response.body);
+    return (albums as List<dynamic>)
+        .map((dynamic album) => Album.fromJson(album as Map<String, dynamic>))
+        .toList();
+  } else {
+    throw Exception('Failed to Load List Album');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controller/controller.dart';
+import 'package:flutter_application_1/data/album.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late Future<List<Album>> futureAlbumList;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbumList = fetchAlbumList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data API',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Album>>(
+            future: futureAlbumList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Text(snapshot.data![index].id.toString()),
+                        title: Text(snapshot.data![index].title),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
